2741ee6559e39594a363ac43e579e82bdb78a62a
MLECAER <malo.lecaer77@gmail.com>
2026-01-29 15:00:23 +0100

    Correction responsive + ajout animation FlipJS

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(Flip);
+
 const buttons = document.querySelectorAll('.nav-btn');
 const cards = document.querySelectorAll('.card');
 const moreContainer = document.getElementById('more-btn-container');
@@ -5,52 +7,59 @@ const moreBtn = document.getElementById('more-btn');
 
 buttons.forEach(btn => {
     btn.addEventListener('click', () => {
-        // Gestion visuelle des boutons (Active / Inactive)
         buttons.forEach(b => b.classList.remove('active'));
         btn.classList.add('active');
-
-        // Récupération du filtre cliqué
+        const targets = [...cards, moreContainer];
+        const state = Flip.getState(targets);
         const filter = btn.getAttribute('data-filter');
 
-        // Boucle sur chaque carte pour appliquer le tri
         cards.forEach(card => {
             const category = card.getAttribute('data-cat');
-
             if (filter === 'all' || category === filter) {
-                // Carte sélectionnée : Visible, nette, taille normale
+                card.style.display = "flex"; 
                 card.style.opacity = '1';
                 card.style.filter = 'grayscale(0%)';
-                card.style.transform = 'scale(1)';
-                card.style.order = '-1'; // Remonte en haut de la grille
-                card.style.pointerEvents = 'auto'; // Réactive le clic
-            } else {
-                // Carte non-sélectionnée : Transparente, grise, plus petite
-                card.style.opacity = '0.2';
-                card.style.filter = 'grayscale(100%)';
-                card.style.transform = 'scale(0.95)';
-                card.style.order = '1'; // Descend en bas
-                card.style.pointerEvents = 'none'; // Empêche le clic
+            } 
+            else {
+                card.style.display = "none"; 
             }
         });
-        // Gestion du bouton "Addicher plus"
+
         if (filter === 'about') {
-            // Si on clique sur "À propos"
             moreContainer.style.display = 'block';
             moreBtn.textContent = 'En savoir plus sur moi →';
-            moreBtn.href = 'about.html'; // Pense à créer cette page
-        } else if (filter === 'work') {
-            // Si on clique sur "Projets"
+            moreBtn.href = 'about.html';
+        } 
+        else if (filter === 'work') {
             moreContainer.style.display = 'block';
             moreBtn.textContent = 'Voir tous les projets →';
-            moreBtn.href = 'projets.html'; // Pense à créer cette page
-        } else {
-            // Pour "*"" ou "Connect", on cache le bouton
+            moreBtn.href = 'projets.html';
+        } 
+        else {
             moreContainer.style.display = 'none';
         }
+
+        Flip.from(state, {
+            duration: 0.7,
+            ease: "power2.inOut",
+            absolute: true, 
+            onEnter: elements => {
+                return gsap.fromTo(elements, 
+                    {opacity: 0, scale: 0}, 
+                    {opacity: 1, scale: 1, duration: 0.7, ease: "power2.inOut"}
+                );
+            },
+            
+            onLeave: elements => {
+                return gsap.to(elements, 
+                    {opacity: 0, scale: 0, duration: 0.7, ease: "power2.inOut"}
+                );
+            }
+        });
     });
 });
 
-// --- ANIMATION TYPEWRITER ---
+// ANIMATION TYPEWRITER 
 const textElement = document.getElementById('bio-text');
 const descElement = document.getElementById('bio-desc');
 const cursorElement = document.querySelector('.cursor');
@@ -64,13 +73,11 @@ function typeWriter() {
     if (i < codePhrase.length) {
         textElement.textContent += codePhrase.charAt(i);
         i++;
-        
-        // Génère un temps aléatoire entre 50ms et 250ms pour chaque lettre
+        // Génère un temps aléatoire entre 50ms et 250ms
         const humanSpeed = Math.floor(Math.random() * 200) + 50;
-        
         setTimeout(typeWriter, humanSpeed); 
-    } else {
-        // Une fois l'écriture finie
+    } 
+    else {
         setTimeout(() => {
             textElement.textContent = finalPhrase;
             descElement.style.opacity = '1';
@@ -80,39 +87,32 @@ function typeWriter() {
 
 window.addEventListener('load', typeWriter);
 
-// --- DARK MODE ---
+// DARK MODE
 const themeSwitch = document.getElementById('theme-switch');
 const body = document.body;
-
-// Vérifier si l'utilisateur avait déjà choisi le mode sombre
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme === 'dark') {
     body.classList.add('dark-mode');
     themeSwitch.checked = true;
 }
 
-// Écouter le changement du switch
 themeSwitch.addEventListener('change', function() {
     if (this.checked) {
         body.classList.add('dark-mode');
-        localStorage.setItem('theme', 'dark'); // Sauvegarde
-    } else {
+        localStorage.setItem('theme', 'dark');
+    } 
+    else {
         body.classList.remove('dark-mode');
-        localStorage.setItem('theme', 'light'); // Sauvegarde
+        localStorage.setItem('theme', 'light');
     }
 });
 
-// --- LIGHTBOX IMAGE ---
-
-// Récupérer les éléments
+// LIGHTBOX IMAGE
 const modal = document.getElementById("image-modal");
 const modalImg = document.getElementById("img-en-grand");
 const captionText = document.getElementById("caption");
 const closeBtn = document.querySelector(".close-modal");
-
-// Cible l'image à l'intérieur de la carte .project-visual
 const projectImage = document.querySelector(".project-visual img");
-
 if (projectImage) {
     projectImage.onclick = function(){
         modal.style.display = "flex";
@@ -121,15 +121,11 @@ if (projectImage) {
         modalImg.src = this.src;
     }
 }
-
-// Fermer la modale quand on clique sur la croix
 if (closeBtn) {
     closeBtn.onclick = function() { 
         modal.style.display = "none";
     }
 }
-
-// Fermer aussi quand on clique en dehors de l'image (sur le fond noir)
 window.addEventListener('click', function(event) {
     if (event.target === modal) {
         modal.style.display = "none";
